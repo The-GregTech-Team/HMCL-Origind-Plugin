d69c87697e3e84c764e6ccbbda3d136e86b88608
LasmGratel <lasm_gratel@hotmail.com>
2018-01-14 11:13:31 +0800

    use nio copy instead of stream

--- src/main/java/cc/origind/server/task/DownloadFileTask.java
+++ src/main/java/cc/origind/server/task/DownloadFileTask.java
@@ -4,15 +4,11 @@ import cc.origind.server.OrigindPlugin;
 import org.jackhuang.hmcl.api.HMCLog;
 import org.jackhuang.hmcl.util.task.TaskInfo;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
+import java.nio.file.StandardCopyOption;
 
 public class DownloadFileTask extends TaskInfo {
     private String file;
@@ -27,13 +23,7 @@ public class DownloadFileTask extends TaskInfo {
         URL url = new URL(OrigindPlugin.getInstance().getConfig().getDownloadPrefix() + file);
         Path to = Paths.get(".minecraft", file).toAbsolutePath();
         HMCLog.log("正在下载: " + url + " 到 " + to);
-        Files.deleteIfExists(to);
-        BufferedWriter writer = Files.newBufferedWriter(to, StandardCharsets.UTF_8);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-        for (String s : reader.lines().collect(Collectors.toList()))
-            writer.append(s);
-        writer.close();
-        reader.close();
+        Files.copy(url.openStream(), to, StandardCopyOption.REPLACE_EXISTING);
         HMCLog.log("下载完毕: " + to);
     }
 }
